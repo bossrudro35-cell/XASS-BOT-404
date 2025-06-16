ce4fa2f28da12e87696c3b6dece5353acbffe1e0
MBBXMESBAH <155101462+MBBXMESBAH@users.noreply.github.com>
2025-06-16 22:20:39 +0600

    Update xass.js

--- scripts/cmds/xass.js
+++ scripts/cmds/xass.js
@@ -12,27 +12,37 @@ module.exports.onStart = ({}) => {};
 
 module.exports.onChat = async ({ api, event, args }) => {
   try {
-    const msg = event.body.toLowerCase();
+    const msg = (event.body || "").toLowerCase();
 
     if (msg === "xass" || msg === "bayjid") {
+      const attachment = await global.utils.getStreamFromURL(
+        "https://i.imgur.com/tCUHoib.jpeg",
+        {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/117.0.0.0 Safari/537.36",
+        }
+      );
+
       api.sendMessage(
         {
-          body: 
-`┏━━━✦✗✦━━━┓
+          body: `┏━━━✦✗✦━━━┓
  𝐗𝐀𝐒𝐒 𝐁𝐎𝐓 𝐇𝐞𝐑𝐞  
 ┗━━━✦✗✦━━━┛
 > Nickname: - BiJu•-🦈🕸️🫀
 > Owner: -𝐗𝐀𝐒𝐒 - 𝐁𝐚𝐘𝐣𝐢𝐝•-🕷️🕸️🫀 (Etx)
 > 𝐗𝐀𝐒𝐒 𝐁𝐎𝐓__/:;)🤍
 🦈🫀`,
-          attachment: await global.utils.getStreamFromURL("https://i.imgur.com/tCUHoib.jpeg", {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/117.0.0.0 Safari/537.36"
-      }),
+          attachment: attachment,
+        },
         event.threadID,
         event.messageID
       );
     }
   } catch (err) {
-    api.sendMessage(`Error: ${err.message}`, event.threadID, event.messageID);
+    api.sendMessage(
+      `Error: ${err.message}`,
+      event.threadID,
+      event.messageID
+    );
   }
 };
